0e642b04e5444ed299d450a21aadd0340bb4a538
Flox <florrr1997@gmail.com>
2018-07-15 01:04:13 -0300

    Me quiero matar

--- assets/js/random.js
+++ assets/js/random.js
@@ -1,10 +1,11 @@
+
 function random() {
   // hay 1920 tiras
   var tiraNum = Math.floor(Math.random() * 1920) + 1;
   var numeroTira = convertirNumero(tiraNum);
   var list = document.getElementsByClassName("iis-slide")[0];
   var hola = document.getElementsByClassName("iis-current-slide")[0];
-  hola.setAttribute('style', "transition-duration: 0ms; background-image: url(\"/historietas/" + numeroTira + ".png\");");
+  hola.setAttribute('style', "transition-duration: 0ms; background-image: url(\"/TodaMafalda/historietas/" + numeroTira + ".png\");");
 }
 
 function convertirNumero(num) {
